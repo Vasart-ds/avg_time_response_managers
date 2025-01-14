16c14db4c39b10844cfeaadab247f9c0037bf936
Artem Vasyukhin <73034094+Vasart-ds@users.noreply.github.com>
2025-01-14 13:47:12 +0300

    Add files via upload

--- data/test_managers_sql.sql
+++ data/test_managers_sql.sql
@@ -1,3 +1,14 @@
+/* Для решения задачи по подчету среднего времени первичного ответа менеджера на обращение клиента мы создадим несколько CTE:
+   
+   - message_sorting: сообщения сортируются по id обращения, попутно избавляясь от некорректных данных, 
+   				      когда created_at = 0 & type = 'outgoing_chat_message';
+   - filtered_msgs:   фильтрация данных с сохранением только первого сообщения клиента и первого ответа менеджера;
+   - response_time:	  расчёт времени ответа менеджера на сообщение с учётом рабочего времени с 9:30 до 00:00. Дни не учитываем, 
+   					  так как предполагается, что поддержка работает каждый день;
+   - avg_response:	  расчёт среднего времени ответа.
+   
+   В итоговом запросе переведем полученные значения в минуты.
+ */
 WITH message_sorting AS (
 	SELECT type,
 		entity_id,
@@ -6,7 +17,7 @@ WITH message_sorting AS (
         	LEAD(created_at) OVER (PARTITION BY entity_id ORDER BY created_at) AS manager_answer_time,
         	LAG(type) OVER (PARTITION BY entity_id ORDER BY created_at) AS prev_type,
         	(CASE
-        		WHEN created_by = 0 AND type = 'outgoing_chat_message' THEN 'uncorrect'
+        		WHEN created_by = 0 AND type = 'outgoing_chat_message' THEN 'uncorrect' --выделение некорректных данных
         	ELSE 'correct'
         	END) as check_messages
 	FROM test.chat_messages
@@ -23,16 +34,16 @@ filtered_msgs AS (
            ELSE prev_type
            END AS upd_type
  	FROM message_sorting
- 	WHERE check_messages != 'uncorrect'
+ 	WHERE check_messages != 'uncorrect' -- очистка некорректных строк, полученных в прошлом CTE
 ),
 response_time AS (
 	SELECT entity_id,
  		created_at,
  		manager_answer_time,
  		(CASE WHEN created_by = 0 THEN lead(created_by) over (partition by entity_id order by created_at) END) AS manager_id,
- 		(CASE WHEN manager_answer_time IS NOT NULL AND created_at IS NOT NULL THEN 
-                	GREATEST(to_timestamp(manager_answer_time)::time, '09:30:00') -
-                	GREATEST(to_timestamp(created_at)::time, '00:00:00') ELSE NULL END) AS response_lag
+ 		(CASE WHEN manager_answer_time IS NOT NULL AND created_at IS NOT NULL then
+ 			(CASE WHEN to_timestamp(created_at)::time < '09:30:00' THEN to_timestamp(manager_answer_time)::time - '09:30:00'::time
+ 					ELSE to_timestamp(manager_answer_time)::time - to_timestamp(created_at)::time END) ELSE NULL END) AS response_lag
 	FROM filtered_msgs
 	WHERE (upd_type = '0' OR upd_type = '1')
 ),
@@ -42,8 +53,8 @@ avg_response AS (
 	   	AVG(rt.response_lag::time) AS avg_response_time,
 	   	r.rop_name
 	FROM response_time rt
-	JOIN test.managers m on rt.manager_id = m.mop_id
-	JOIN test.rops r on r.rop_id = m.rop_id::int
+	JOIN test.managers m on rt.manager_id = m.mop_id --объединяем таблицы для получения имен менеджеров 
+	JOIN test.rops r on r.rop_id = m.rop_id::int -- объединяем таблицы для получения имен РОП 
 	WHERE manager_id is not null and manager_id != 0
 	GROUP BY rt.manager_id, m.name_mop, r.rop_name
 )
@@ -53,7 +64,7 @@ SELECT manager_id,
 	rop_name,
 	CASE 
 	   WHEN EXTRACT(HOUR FROM avg_response_time::time) > 0 THEN 
-		 	 EXTRACT(HOUR FROM avg_response_time::time) * 60 + extract(MINUTE FROM avg_response_time::time)
+		 	EXTRACT(HOUR FROM avg_response_time::time) * 60 + extract(MINUTE FROM avg_response_time::time)
 	   ELSE EXTRACT(MINUTE FROM avg_response_time::time)
 	   END AS avg_response_minutes
 FROM avg_response
